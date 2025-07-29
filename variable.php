b627204dded8b0eafb4062115b020fa482bee179
Kamrul Hasan Shuvo <57871878+shuvokhasan@users.noreply.github.com>
2025-07-29 20:46:28 +0600

    Add files via upload

---
+++ variable.php
@@ -0,0 +1,35 @@
+<?php
+
+// Start with $ – All variables begin with a dollar sign: $var.
+
+// $x = 3;
+// echo $x;
+
+// Valid names – Variable names must start with a letter or underscore (_), followed by any number of letters, numbers, or underscores.
+
+$var = 1;
+$Var = 13;
+
+// echo $Var;
+// echo '<br />';
+// echo $var;
+
+// Case-sensitive – $name and $Name are different variables.
+
+// echo $Var;
+// echo '<br />';
+// echo $var;
+
+// No spaces – Use underscores (_) or camelCase instead of spaces
+
+$vAriable = 'Hello';
+echo $vAriable;
+
+// No special characters – Avoid characters like !, @, #, -, etc.
+
+// Must be assigned before use – Variables must be assigned a value before being used.
+
+// Dynamically typed – PHP variables do not need a type declaration.
+
+
+?>
