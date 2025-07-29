2a80c8823a06876a601de66e89c03feaddde7225
Kamrul Hasan Shuvo <57871878+shuvokhasan@users.noreply.github.com>
2025-07-30 02:21:25 +0600

    Add files via upload

---
+++ data-types.php
@@ -0,0 +1,23 @@
+<?php
+
+    // $integer = 98;
+    // echo $integer;
+
+    // $float = 98.97;
+    // echo $float;
+
+    // $string = "A str'ing";
+    // echo $string;
+    // $string2 = 'Another string';
+    // echo $string2;
+    // $string3 = "Also a string";
+    // echo $string3;
+    // $string4 = '98.97'; //Etao string 🤦
+    // // echo $string4;
+    
+    // echo var_dump($string4);
+
+    $boolean = true;
+    echo $boolean;
+
+?>
