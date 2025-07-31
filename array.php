8763ca8cd9aeac706516c70a4ae4d2a55d670c45
Kamrul Hasan Shuvo <57871878+shuvokhasan@users.noreply.github.com>
2025-07-31 22:21:47 +0600

    Add files via upload

--- array.php
+++ array.php
@@ -4,37 +4,33 @@
     $fruits = array('apple', 'banana', 'mango', 'orange', 'olive');
     //                 0        1         2         3        4
 
-    $fruit ='Apple';
-
-    // $countries = ['Bangladesh', 'Myanmar', 'India', 'Nepal', 'Bhutan', 'China',];
-
-    // echo $fruits[2];
-
-    // echo var_dump($fruits);
-    // echo '<br /><br />';
-    // print_r($countries);
-
-    // $numbers = [3, 5, 8, 10, 23];
-    // echo max($numbers);
-    // echo min($numbers);
-    
-    // echo max($countries);
-    // echo min($countries);
-
-    // Array Mutation (Arrays are mutable)
-    // $countries[1] = 'Srilanka';
-    // print_r($countries);
-
-    // echo '<br /><br />';    
 
     $countries = ['Bangladesh', 'Myanmar', 'India', 'Nepal', 'Bhutan', 'China',];
 
+    echo $fruits[2];
+
+    echo var_dump($fruits);
+    echo '<br /><br />';
+    print_r($countries);
+
+    $numbers = [3, 5, 8, 10, 23];
+    echo max($numbers);
+    echo min($numbers);
+    
+    echo max($countries);
+    echo min($countries);
+
+    // Array Mutation (Arrays are mutable)
+    $countries[1] = 'Srilanka';
+    print_r($countries);
+
+    echo '<br /><br />';    
+
     // Adding values to existing array
-    // $countries[] = 'America';
-    // print_r($countries);
+    $countries[] = 'America';
+    print_r($countries);
 
     // Removing values from existing array
-    // unset( );
     unset($countries[3]);
     print_r($countries);
 
