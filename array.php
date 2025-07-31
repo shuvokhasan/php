a4b7c49f4190aac517bdd3cf04cf16e7e11b171d
Kamrul Hasan Shuvo <57871878+shuvokhasan@users.noreply.github.com>
2025-07-31 22:19:34 +0600

    Add files via upload

--- array.php
+++ array.php
@@ -1,8 +1,47 @@
 <?php
-    
-    $cars = ['Toyota', 'Volvo', 'Ferrari'];
 
-    foreach($car in $cars){
-        
-        echo $cars[$car];
-    }
+    // Array is an idexed variable, starts form 0
+    $fruits = array('apple', 'banana', 'mango', 'orange', 'olive');
+    //                 0        1         2         3        4
+
+    $fruit ='Apple';
+
+    // $countries = ['Bangladesh', 'Myanmar', 'India', 'Nepal', 'Bhutan', 'China',];
+
+    // echo $fruits[2];
+
+    // echo var_dump($fruits);
+    // echo '<br /><br />';
+    // print_r($countries);
+
+    // $numbers = [3, 5, 8, 10, 23];
+    // echo max($numbers);
+    // echo min($numbers);
+    
+    // echo max($countries);
+    // echo min($countries);
+
+    // Array Mutation (Arrays are mutable)
+    // $countries[1] = 'Srilanka';
+    // print_r($countries);
+
+    // echo '<br /><br />';    
+
+    $countries = ['Bangladesh', 'Myanmar', 'India', 'Nepal', 'Bhutan', 'China',];
+
+    // Adding values to existing array
+    // $countries[] = 'America';
+    // print_r($countries);
+
+    // Removing values from existing array
+    // unset( );
+    unset($countries[3]);
+    print_r($countries);
+
+    // Re-indexing array after rermoving
+    $countries = array_values($countries);
+    echo '<br /><br />';
+    print_r($countries);
+
+?>
+    
